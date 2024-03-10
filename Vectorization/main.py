f00195099862a9e3a1cf88eeaed2b8a82f701aa9
Misash <misash@outlook.com>
2024-03-10 18:32:19 -0500

    reorganize vectorization logic

--- Vectorization/main.py
+++ Vectorization/main.py
@@ -6,19 +6,19 @@ load_dotenv(find_dotenv())
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-
+from langchain_pinecone import PineconeVectorStore
 
 # get data from pdf
+from langchain_community.document_loaders import PyPDFLoader
 import re, os
 import pinecone
 from pinecone import Pinecone, ServerlessSpec
 from pinecone import Pinecone, PodSpec
+import base64
+import time
 
-# connect with pinecone 
-pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
-
-def createIndexPinecone(indexName):
+def createIndexPinecone(indexName,pc):
     # pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
     indexFound = indexName in pc.list_indexes().names()
 
@@ -37,38 +37,73 @@ def createIndexPinecone(indexName):
     return indexFound
 
 
-def deleteIndexPinecone(indexName):
+def deleteIndexPinecone(indexName,pc):
     pc.delete_index(indexName)
 
 
 
+def storeEmbeddingsPinecone(pdfpath,index_name):
+
+    # get text from pdf
+    loader = PyPDFLoader(pdfpath)
+    documents = loader.load()
+    print("reading Pdf...")
+
+    # split pdf on chunks
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=0,
+    )
+    docs = text_splitter.split_documents(documents)
+    print("num of chunks: ",len(docs))
+
+    #turn the chunks on embeddings
+    embeddings = OpenAIEmbeddings()
+    # query_result = embeddings.embed_query(docs[0].page_content)
+    # print(query_result)
+
+    # code pdf name to store as namespace on vectordb
+    pdfNameFile = os.path.basename(pdfpath) 
+    pdfNameFile = os.path.splitext(pdfNameFile)[0] 
+    timestamp = str(time.time())
+    uniqueName = pdfNameFile + "_" + timestamp
+    encodedName = base64.urlsafe_b64encode(uniqueName.encode()).decode()
+
+    # decodedBytes = base64.urlsafe_b64decode(encodedName)
+    # decodedName = decodedBytes.decode()
+
+    #store embeddings on namespace Pinecone Vectore Store
+    docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name, namespace=encodedName)
 
 
-# Create index on Pinecone
-indexName = "starter-index"
-# createIndexPinecone(indexName)
+# connect with pinecone 
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+
+pdf_path = "./pdf/naruto.pdf"
+index_name = "starter-index"
 
 
-# Create an unique namespace for pdf
-# index = pc.Index(indexName)
-# Upsert records while creating a new namespace
-# index.upsert(vectors=[('id-1', [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])],namespace='my-first-namespace')
+storeEmbeddingsPinecone(pdf_path,index_name)
 
 
 
-# get text from pdf
-# from langchain_community.document_loaders import PyPDFLoader
-# loader = PyPDFLoader("./pdf/naruto.pdf")
-# loader = TextLoader("./data/enterprise_data.txt")
-# documents = loader.load()
 
-# split pdf on chunks
-# text_splitter = CharacterTextSplitter(
-#     separator="\n",
-#     chunk_size=1000,
-#     chunk_overlap=0,
-# )
-# docs = text_splitter.split_documents(documents)
+
+
+
+# pdfNameFile = os.path.basename(pdfpath) 
+# pdfNameFile = os.path.splitext(pdfNameFile)[0] 
+# timestamp = str(time.time())
+# print("pdf name: ",pdfNameFile)
+# uniqueName = pdfNameFile + "_" + timestamp
+# print("uniqueName: ",uniqueName)
+# encodedName = base64.urlsafe_b64encode(uniqueName.encode()).decode()
+# print("encoded: ", encodedName)
+# decodedBytes = base64.urlsafe_b64decode(encodedName)
+# decodedName = decodedBytes.decode()
+# print("dncoded: ", decodedName)
+
 
 # print chunks
 # for i in range(len(docs)):
@@ -82,21 +117,21 @@ indexName = "starter-index"
 # query_result = embeddings.embed_query(docs[0].page_content)
 # print(query_result)
 
+
 #store embeddings on Pinecone
 # from langchain_pinecone import PineconeVectorStore
 # index_name = "starter-index"
 # docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name, namespace="naruto")
 
 
-
 # vector similarity search
-query = "tell me about naruto"
-# result  = docsearch.similarity_search(query)
-result  = docsearch.similarity_search(
-    namespace="naruto",
-    query = query
-)
-print(result [0].page_content)
+# query = "tell me about naruto"
+# # result  = docsearch.similarity_search(query)
+# result  = docsearch.similarity_search(
+#     namespace="naruto",
+#     query = query
+# )
+# print(result [0].page_content)
 
 
 # def answer_question(question):
